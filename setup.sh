3606338c58602318d4f46e8c9aedc989de982e1f
Manu Miu <miu.manu@gmx.de>
2015-10-09 07:55:44 -0400

    added setup.sh as install script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git --config user.name "Manu Miu"
+git --config user.email "miu.manu@gmx.de"
+
+sudo ln -s $HOME/.homesick/repos/homeshick/completions/homeshick.fish /usr/share/fish/completions/homeshick.fish
