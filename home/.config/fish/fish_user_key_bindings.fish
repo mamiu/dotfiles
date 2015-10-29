aba8b4663fd88b3b30b3ad1a620d5f3043227403
Manu Miu <miu.manu@gmx.de>
2015-10-29 09:01:12 +0100

    autostart tmux on login

--- home/.config/fish/fish_user_key_bindings.fish
+++ home/.config/fish/fish_user_key_bindings.fish
@@ -2,7 +2,6 @@ function fish_user_key_bindings
     # fish_vi_key_bindings
     # bind -M insert \e\n accept-autosuggestion execute
     bind \e\n accept-autosuggestion execute
-    bind \e\n accept-autosuggestion execute
     bind \ck history-search-backward
     bind \e\[I 'begin;end'
     bind \e\[O 'begin;end'
