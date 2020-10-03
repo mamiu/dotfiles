6b5a6dbd6b0a42af92482bcf2c5b93a8217753d8
Manu Miu <miu.manu@gmx.de>
2020-10-03 19:33:16 +1000

    add bat and htop config and new keybindings

--- home/.config/fish/fish_user_key_bindings.fish
+++ home/.config/fish/fish_user_key_bindings.fish
@@ -1,7 +1,10 @@
 function fish_user_key_bindings
-    bind \e\r accept-autosuggestion execute
+    bind \e\r accept-autosuggestion repaint execute
     bind \el 'commandline -a la; commandline -f execute'
     bind \ck history-search-backward
+    bind \cJ fzf-cd-widget
+    bind \ew 'commandline -b "watch -n 1 $history[1]"; commandline -f repaint; commandline -f execute'
+    # temporary fix for a bug where strange characters are inserted when scrolling
     bind \e\[I 'begin;end'
     bind \e\[O 'begin;end'
 end
