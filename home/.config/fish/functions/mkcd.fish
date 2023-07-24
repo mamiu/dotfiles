b34bf6306d4c44a2b5d53c4cbe6e061f11cdba74
Paul Miu <miu.manu@gmx.de>
2023-07-24 16:55:41 -0500

    create nested folders with mkcd if path is provided

--- home/.config/fish/functions/mkcd.fish
+++ home/.config/fish/functions/mkcd.fish
@@ -3,7 +3,7 @@
 # Combination of "mkdir" and "cd"
 function mkcd
   # First create a folder for each argument provided
-  mkdir $argv
+  mkdir -p $argv
   # If the creation of folders was successful change into the first one
   and cd "$argv[1]"
 end
